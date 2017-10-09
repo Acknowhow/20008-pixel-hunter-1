dfbeb0a37875ace36033482fa3aad499212629ff
Vadim Korabelnik <vad@oblako9.ru>
2017-10-10 01:01:49 +0300

    Function working both ways, need to measure while index is at 0

--- js/main.js
+++ js/main.js
@@ -1,37 +1,38 @@
 (function () {
-  let central = document.querySelector(`.central`);
-  let main = document.querySelector(`#main`);
-  let footer = document.querySelector(`.central .footer`);
-  let greeting = document.querySelector(`#greeting`);
-  let temps = document.querySelectorAll(`template`);
+  const central = document.querySelector(`.central`);
+  const intro = document.querySelector(`#intro`);
+  const temps = document.querySelectorAll(`template`);
   let defaultIndex = 0;
   let tempsArr = [];
   let keysMap = [];
   Array.prototype.push.apply(tempsArr, temps);
-  tempsArr = tempsArr.map(function (temp) {
-  // Pushing all links Next and Back to temps array
-    return temp.content.querySelectorAll(`img[alt='Next'], img[alt='Back']`);
-  });
+  // Put intro at 0 index
+  tempsArr.unshift(intro);
   function show(slide) {
-    let mainArr = [main, footer];
     let content = slide.content;
     // Clone template content
     let clone = document.importNode(content, true);
-    mainArr.forEach(function (selector) {
-      selector.style.display = `none`;
-    });
+    while (central.firstChild) {
+      central.removeChild(central.firstChild);
+    }
     central.appendChild(clone);
   }
   function keyDownHandler(ev) {
     ev = ev || event; // to deal with IE
     keysMap[ev.keyCode] = ev.type === `keydown`;
     if (keysMap[18] && keysMap[39]) {
-      show(temps[defaultIndex++]);
+      show(temps[defaultIndex++ % temps.length]); // Measure default index
+      keysMap = {};
+    }
+    if (keysMap[18] && keysMap[37]) {
+      show(temps[defaultIndex-- % temps.length]);
       keysMap = {};
     }
   }
-  console.log(temps);
-  document.addEventListener(`keydown`, keyDownHandler, false);
+  document.addEventListener(`keydown`, function (ev) {
+    keyDownHandler(ev);
+  });
   // Greeting
+  console.log(tempsArr);
 })();
 
