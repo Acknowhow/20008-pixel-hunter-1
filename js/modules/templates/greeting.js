23115dce6d6bf52e450f410ea05c8aa70b2666b5
Vadim Korabelnik <vad@oblako9.ru>
2017-10-10 17:48:24 +0300

    Updated main.js

---
+++ js/modules/templates/greeting.js
@@ -0,0 +1,28 @@
+import {templateFooter} from './footer.js';
+import {centralContainer} from '../module-constructor';
+import {makeTemplate} from '../module-constructor.js';
+import {makeRulesTemplate} from './rules';
+export const moduleGreeting = `<div class="greeting central--blur">
+    <div class="greeting__logo"><img src="img/logo_big.png" width="201" height="89" alt="Pixel Hunter"></div>
+    <h1 class="greeting__asterisk">*</h1>
+    <div class="greeting__challenge">
+      <h3>Лучшие художники-фотореалисты бросают&nbsp;тебе&nbsp;вызов!</h3>
+      <p>Правила игры просты.<br>
+        Нужно отличить рисунок&nbsp;от фотографии и сделать выбор.<br>
+        Задача кажется тривиальной, но не думай, что все так просто.<br>
+        Фотореализм обманчив и коварен.<br>
+        Помни, главное — смотреть очень внимательно.</p>
+    </div>
+    <div class="greeting__continue"><span><img src="img/arrow_right.svg" width="64" height="64" alt="Next"></span></div>
+  </div>`;
+export const makeGreetingTemplate = () => {
+  makeTemplate(moduleGreeting, templateFooter);
+  const next = (ev) => {
+    if (ev.target === document.querySelector(`img[alt='Next']`)) {
+      central.removeEventListener(`click`, next);
+      makeRulesTemplate();
+    }
+  };
+  central.addEventListener(`click`, next);
+};
+
