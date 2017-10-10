95b9a2f81c5774a014ea7f88af23bba6c6d932d2
Vadim Korabelnik <vad@oblako9.ru>
2017-10-10 16:27:50 +0300

    Added intro module

--- js/modules/templates/intro.js
+++ js/modules/templates/intro.js
@@ -1,2 +1,20 @@
 import {templateFooter} from './footer.js';
 import {centralContainer} from '../module-constructor';
+import {makeTemplate} from "../module-constructor";
+import {makeGreetingTemplate} from './greeting.js';
+const contentIntro = `<div id="main" class="central__content">
+    <div class="intro">
+      <h1 class="intro__asterisk">*</h1>
+      <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+    </div>
+  </div>`;
+export const makeIntroTemplate = () => {
+  makeTemplate(contentIntro, templateFooter);
+  let next = (ev) => {
+    if (ev.target === document.querySelector(`.intro__asterisk`)) {
+      centralContainer.removeEventListener(`click`, next);
+      makeGreetingTemplate();
+    }
+  };
+  centralContainer.addEventListener(`click`, next);
+};
