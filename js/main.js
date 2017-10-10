fe68e4a50c31328faa11f0eb058ef382a8dabbf7
Vadim Korabelnik <vad@oblako9.ru>
2017-10-10 10:24:40 +0300

    Removed keyMap

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
   const centralContainer = document.querySelector(`.central`);
   const templates = Array.from(document.querySelectorAll(`template`));
   let defaultIndex = 0;
-  let keysMap = [];
   function show(slide) {
     const content = slide.content;
     const clone = content.cloneNode(true);
@@ -10,23 +9,21 @@
     centralContainer.appendChild(clone);
   }
   function keyDownHandler(ev) {
-    keysMap[ev.keyCode] = ev.type;
     const KEY_CODE_RIGHT = 39;
     const KEY_CODE_LEFT = 37;
-    if (ev.altKey && keysMap[KEY_CODE_RIGHT]) {
+    if (ev.altKey && ev.keyCode === KEY_CODE_RIGHT) {
       defaultIndex++;
       if (defaultIndex % templates.length === 0) {
         defaultIndex--;
       }
     }
-    if (ev.altKey && keysMap[KEY_CODE_LEFT]) {
+    if (ev.altKey && ev.keyCode === KEY_CODE_LEFT) {
       defaultIndex--;
       if (defaultIndex === -1) {
         defaultIndex++;
       }
     }
     show(templates[defaultIndex]);
-    keysMap = [];
   }
   document.addEventListener(`keydown`, keyDownHandler);
   show(templates[defaultIndex]);
