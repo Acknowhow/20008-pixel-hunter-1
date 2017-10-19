e2e1c211fea25ed73df5c13906ff704f5221070b
Vadim Korabelnik <vad@oblako9.ru>
2017-10-20 01:05:49 +0300

    Adding events to rules module

--- js/rules/rules-view.js
+++ js/rules/rules-view.js
@@ -27,6 +27,46 @@ export default class RulesView extends ModuleView {
     <input class="rules__input" type="text" placeholder="Ваше Имя">
     <button class="rules__button  continue" type="submit" disabled>Go!</button>
   </form>
-</div>`;
+</div>`.trim();
+  }
+
+  bind() {
+    const input = this.element.querySelector(`.rules__input`);
+    const button = this.element.querySelector(`.rules__button`);
+
+    const linkBack = this.element.querySelector(`img[alt='Back']`);
+    // linkBack.onclick =
   }
 }
+
+// export const makeRulesTemplate = () => {
+//   const el = makeTemplate(moduleRules);
+//   const rulesInput = el.querySelector(`.rules__input`);
+//
+//   const rulesButton = el.querySelector(`.rules__button`);
+//   const linkBack = el.querySelector(`img[alt='Back']`);
+//
+//   const switchBack = () => {
+//     linkBack.removeEventListener(`click`, switchBack);
+//     const introTemplate = makeIntroTemplate();
+//     insertIntoContainer(introTemplate);
+//   };
+//
+//   const enable = () => {
+//     rulesButton.removeAttribute(`disabled`);
+//   };
+//
+//   const next = () => {
+//     rulesInput.removeEventListener(`input`, enable);
+//     rulesButton.removeEventListener(`click`, next);
+//
+//     const game1Template = makeGame1Template();
+//     insertIntoContainer(game1Template);
+//   };
+//   rulesInput.addEventListener(`input`, enable);
+//   rulesButton.addEventListener(`click`, next);
+//
+//   linkBack.addEventListener(`click`, switchBack);
+//   return el;
+// };
+
--- js/rules/rules.js
+++ js/rules/rules.js
@@ -4,33 +4,3 @@ const rules = new RulesView();
 
 export default () => rules;
 
-// export const makeRulesTemplate = () => {
-//   const el = makeTemplate(moduleRules);
-//   const rulesInput = el.querySelector(`.rules__input`);
-//
-//   const rulesButton = el.querySelector(`.rules__button`);
-//   const linkBack = el.querySelector(`img[alt='Back']`);
-//
-//   const switchBack = () => {
-//     linkBack.removeEventListener(`click`, switchBack);
-//     const introTemplate = makeIntroTemplate();
-//     insertIntoContainer(introTemplate);
-//   };
-//
-//   const enable = () => {
-//     rulesButton.removeAttribute(`disabled`);
-//   };
-//
-//   const next = () => {
-//     rulesInput.removeEventListener(`input`, enable);
-//     rulesButton.removeEventListener(`click`, next);
-//
-//     const game1Template = makeGame1Template();
-//     insertIntoContainer(game1Template);
-//   };
-//   rulesInput.addEventListener(`input`, enable);
-//   rulesButton.addEventListener(`click`, next);
-//
-//   linkBack.addEventListener(`click`, switchBack);
-//   return el;
-// };
