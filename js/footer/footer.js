cd1a9b5b577b75697f7bc199893932b88f8f929e
Vadim Korabelnik <vad@oblako9.ru>
2017-10-27 05:43:39 +0300

    Footer added

---
+++ js/footer/footer.js
@@ -0,0 +1,44 @@
+const date = new Date();
+const year = date.getFullYear();
+
+const socialLinks = [`Твиттер`, `Инстаграм`, `Фэйсбук`, `Вконтакте`];
+
+const footer = {
+  credits: {
+    author: `HTML Academy`,
+    text: `Сделано в`,
+    src: `https://htmlacademy.ru`,
+    date: [year]
+  },
+  social: {
+    [socialLinks[0]]: {
+      src: `https://twitter.com/htmlacademy_ru`,
+      modifier: `tw`
+    },
+    [socialLinks[1]]: {
+      src: `https://www.instagram.com/htmlacademy/`,
+      modifier: `ins`
+    },
+    [socialLinks[2]]: {
+      src: `https://www.facebook.com/htmlacademy`,
+      modifier: `fb`
+    },
+    [socialLinks[3]]: {
+      src: `https://vk.com/htmlacademy`,
+      modifier: `vk`
+    }
+  }
+};
+
+const socialData = Object.keys(footer.social);
+const socialLink = socialData.map((link, value) => ({value: socialData[value], link: footer.social[link]}));
+
+const FooterDefault = () => {
+  return `<a href="${footer.credits.src}" class="social-link social-link--academy">${footer.credits.author}</a>
+    <span class="footer__made-in">Сделано в <a href="${footer.credits.src}" class="footer__link">${footer.credits.author}</a> &copy; ${footer.credits.date}</span>
+       ${socialLink.map(({value, link}) => `<a href="${link.src}" class="social-link social-link--${link.modifier}>${value}</a>`).join(``)} 
+    <div class="footer__social-links"> 
+</div>`.trim();
+};
+
+export default () => FooterDefault;
