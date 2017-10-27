b7bc433a415c29703ab0d9e37fc981591000a9e0
Vadim Korabelnik <vad@oblako9.ru>
2017-10-27 05:56:37 +0300

    Adding header

--- js/footer/footer.js
+++ js/footer/footer.js
@@ -35,7 +35,7 @@ const socialLink = socialData.map((link, value) => ({value: socialData[value], l
 
 const FooterDefault = () => {
   return `<a href="${footer.credits.src}" class="social-link social-link--academy">${footer.credits.author}</a>
-    <span class="footer__made-in">Сделано в <a href="${footer.credits.src}" class="footer__link">${footer.credits.author}</a> &copy; ${footer.credits.date}</span>
+    <span class="footer__made-in">${footer.credits.text}<a href="${footer.credits.src}" class="footer__link">${footer.credits.author}</a> &copy; ${footer.credits.date}</span>
        ${socialLink.map(({value, link}) => `<a href="${link.src}" class="social-link social-link--${link.modifier}>${value}</a>`).join(``)} 
     <div class="footer__social-links"> 
 </div>`.trim();
